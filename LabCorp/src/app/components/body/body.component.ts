2b87cb7f1a78e2b9a9719c2d9d01d22c1f7b163f
vrsheen <vrsheen1@gmail.com>
2017-11-26 19:25:10 -0500

    Alignment Updates

--- LabCorp/src/app/components/body/body.component.ts
+++ LabCorp/src/app/components/body/body.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-body',
-  templateUrl: './body.component.html'
+  templateUrl: './body.component.html',
+  styleUrls: ['./body.component.css'],
 })
 export class BodyComponent implements OnInit {
 
